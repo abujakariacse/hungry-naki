82f66ae309b18dea54147c3201687d7182ac9626
abujakariacse <abujakaria316@gmail.com>
2022-03-25 23:02:20 +0600

    file ready

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='heading'>Hungry Naki?</h1>
+            <p className='sub-heading'>E-Food Delivery System</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Meals = () => {
+    return (
+        <div> 
+        </div>
+    );
+};
+
+export default Meals;
